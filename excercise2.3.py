78f75079e24a2d0554281855c6a8ca3a10e74654
konstantinkovalev <konstkov@metropolia.fi>
2024-08-22 10:42:40 +0300

    Program that calculates rectangle's perimeter and area based on the values provided.

---
+++ excercise2.3.py
@@ -0,0 +1,8 @@
+
+length_str=input("Enter the length of the rectangle : ")
+length=float(length_str)
+width_str=input("Enter the width of the rectangle : ")
+width=float(width_str)
+print(f"The perimeter of the rectangle is: {length*2+width*2}")
+area = width * length
+print(f"The area of the rectangle is: {area:.2f}")
