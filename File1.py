8f658592070c4d7dd5319cd91b45f6aaf78e00f2
konstantinkovalev <konstkov@metropolia.fi>
2024-08-22 09:34:08 +0300

    solution

---
+++ File1.py
@@ -0,0 +1,94 @@
+"""
+ # number = input("give me a number")
+ print(number)
+ number_of_year = input ("Input a year: ")
+ """
+"""
+print(number_of_year)
+"""
+"""
+name = input ("your name")
+print ("Hello" + name)
+
+int_var = 3
+float_var = 23.8
+string_var = "Bla"
+print(type(int_var))
+print(type(float_var))
+print(type(string_var))
+
+name = input ("Name: ")
+print(name)
+name = name + " bla "
+print(name)
+"""
+"""
+result = 18/7.5
+print(result)
+print(type(result))
+rounded_result=int(result)
+print(rounded_result)
+number1 = int(input("Enter a number: "))
+number2 = int(input("Enter another number: "))
+print(number1 * number2)
+print(3+7)
+print(3//2)
+print("2+3")
+"""
+"""
+num = "10"
+print("thiss is a number" + str(num))
+print("this is a number", num)
+print(f"this is a number {num}")
+num2 = 1/3
+print(num2)
+print(f"this is the new num {num2:.2f}")
+"""
+"""
+name = input ("Please type in a name")
+year = input ("Please type in a year")
+print(name + " is a valiant knight, born in the year" + year + " One morning" + name + " woke up and etc. etc.")
+"""
+"""
+days = int(input("How many days?"))
+days_in_seconds = (days * 86400)
+print(days_in_seconds)
+"""
+"""
+times = int(input("How many times you eat a week?"))
+price = float(input("What's the price?"))
+spent = float(input("How much money do you spend on groceries?"))
+"""
+"""
+print("Average food expenses")
+print(f"Daily: {(times*price+spent)/7}")
+"""
+print(f"{3*5}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
