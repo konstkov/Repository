64cf5fd7d8c7365c26eab57e369ff584da8dceff
konstantinkovalev <konstkov@metropolia.fi>
2024-08-24 17:51:03 +0200

    Program that converts the mass expressed in various medieval units into kilograms and grams.

--- excercise2.5.py
+++ excercise2.5.py
@@ -1,12 +1,13 @@
 print("Enter a mass in medieval units: ")
 talents_str=input("Talents: ")
 talents=float(talents_str)
+print(f"= {talents*20*32*0.0133:.0f}  kilograms and {(talents*20*32*0.0133) % 1000:.2f}  grams")
 pounds_str=input("Pounds: ")
 pounds=float(pounds_str)
+print(f"= {pounds*32*0.0133:.0f}  kilograms and {(pounds*32*0.0133) % 1000:.2f} grams")
 lots_str=input("Lots: ")
 lots=float(lots_str)
-print(f"Based on the mass you provided in talents, it would be {(talents/20/32)*0.0133:.2f} in kilograms")
-print(f"Based on the mass you provided in pounds, it would be {(pounds/32)*0.0133:.2f} in kilograms")
-print(f"Based on the mass you provided in lots, it would be {lots*0.0133:.2f} in kilograms")
+print(f"= {lots*0.0133:.0f}  kilograms and {(lots*0.0133) % 1000:.2f} grams")
+
 
 
