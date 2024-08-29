c196948d3b3559378a6944d5a61d7447b22646f7
konstantinkovalev <konstkov@metropolia.fi>
2024-08-29 10:24:19 +0300

    programs that tells the user to release the fish back in to the lake, if it's smaller than 42 cm based on the value the user has provided.

---
+++ excercise 3.1.py
@@ -0,0 +1,5 @@
+zander=int(input("Enter the length of zander"))
+if zander < 42:
+    print(f"You have to release the fish back into the lake, the fish needs to be at least {42 - zander} cm longer")
+else:
+    print("You have to eat this fish")
