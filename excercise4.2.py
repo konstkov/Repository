cdab184d7e00faf50e26a6a22dde231fa2c3d378
konstantinkovalev <konstkov@metropolia.fi>
2024-09-02 11:57:03 +0300

    program that converts inches to centimeters based on the values provided by the user.

---
+++ excercise4.2.py
@@ -0,0 +1,8 @@
+while True:
+    value = input("Enter the value in inches:")
+    inches = float(value)
+    if inches >= 0:
+         print(f"The value you have provided equals {inches * 2.54} centimeters")
+
+
+
