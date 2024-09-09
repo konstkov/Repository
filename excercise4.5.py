922fe567022f7dd0b6a9b5a7138199381f69bac9
konstantinkovalev <konstkov@metropolia.fi>
2024-09-09 13:04:22 +0300

    Program that asks the user for a username and password. If either or both are incorrect, the program ask the user to enter the username and password again. This continues until the login information is correct or wrong credentials have been entered five times. If the information is correct, the program prints out Welcome. After five failed attempts the program prints out Access denied.

---
+++ excercise4.5.py
@@ -0,0 +1,17 @@
+quantity_username=[]
+quantity_password=[]
+username="python"
+password="rules"
+while True:
+    user_username=input("Enter your username: ")
+    user_password=input("Enter your password: ")
+    quantity_username.append(user_username)
+    quantity_password.append(user_password)
+    if user_username==username and user_password==password:
+        print("Welcome!")
+        break
+    elif len(quantity_username)>=5 or len(quantity_password)>=5:
+        print("Access denied")
+        break
+    else:
+        print("Please try again")
